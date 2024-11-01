36ae9386b469547e69b9903a6e9d4a6584403166
syed7086 <sa1454101@gmail.com>
2024-11-01 18:34:51 +0530

    Re-implemented Task Management

--- Task Management/script.js
+++ Task Management/script.js
@@ -1,42 +1,41 @@
 let inputBox = document.getElementById('input-box');
 let listContainer = document.getElementById('list-container');
 
-const addTask = () => {
+const  addTask = () => {
     if (inputBox.value === '') {
-        alert('Enter Something in the given field!');
+        alert('Add some Input Text!');
     } else {
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
-        listContainer.appendChild(li)
+        listContainer.appendChild(li);
         let span = document.createElement('span');
         span.innerHTML = '\u00d7';
         li.appendChild(span);
-    }
+    };
     inputBox.value = '';
     saveData();
 };
 
-// Adding Checked and Delete functionality
 
-listContainer.addEventListener('click', (e)=> {
+listContainer.addEventListener('click', (e) => {
     const target = e.target;
-    if (target.matches('li')){
+    if (target.matches('LI')){
         target.classList.toggle('checked');
         saveData();
-    } else if (target.matches('span')) {
+    } else if (target.matches('span')){
         target.closest('li').remove();
         saveData();
-    }
+    };
 });
 
-// Local Storage for storing
+// Local Storage saving
 
 const saveData = () => {
     localStorage.setItem('data', listContainer.innerHTML);
-};
+}
 
 const showData = () => {
     listContainer.innerHTML = localStorage.getItem('data');
-};
+}
 
 showData();
